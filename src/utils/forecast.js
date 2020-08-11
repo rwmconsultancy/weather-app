5881052b5c3a0380e8be2c4d639800c67cc2a898
rwmconsultancy <48714524+rwmconsultancy@users.noreply.github.com>
2020-08-11 08:21:51 +0200

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('postman-request')
+
+const forecast = (longitude, latitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=77513038049c95c435fc51ad460d3d81&query=' + longitude + ',' + latitude + '&units=m'
+    // console.log(url)
+    request({ url, json: true}, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather services', undefined)
+        } else if (body.error) {
+            callback(body.error.info, undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + '. The outside temperature is ' + body.current.temperature + ' and it feels like ' + body.current.feelslike + ' degrees')
+        }
+
+    })
+}
+
+module.exports = forecast
