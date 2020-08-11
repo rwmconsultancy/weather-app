96544a4d94bb2eaf5397f22e4e4931d011efe3db
rwmconsultancy <48714524+rwmconsultancy@users.noreply.github.com>
2020-08-11 09:15:04 +0200

    test 2

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1') // # is voor ID
-// const messageTwo = document.querySelector('#message-2') 
+const messageTwo = document.querySelector('#message-2') 
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
